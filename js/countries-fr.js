3b552798f69858545142b450f1364cc369616b70
Mathieu Forest <forestmathieu@gmail.com>
2014-11-28 13:08:32 -0500

    Create countries-fr.js

---
+++ js/countries-fr.js
@@ -0,0 +1,48 @@
+// Countries
+var country_arr = new Array("Canada", "\311tats-Unis", "Autres");
+
+// States
+var s_a = new Array();
+s_a[0] ="";		
+s_a[1] ="Alberta|Colombie-Britanique|Manitoba|Nouveau-Brunswick|Terre-Neuve|Territoire du Nord-Ouest|Nouvelle-\311cosse|Nunavut|Ontario|\316le-Prince-Edward|Qu\351bec|Saskatchewan|Territoire du Yukon";		
+s_a[2] ="Alabama|Alaska|Arizona|Arkansas|California|Colorado|Connecticut|Delaware|District of Columbia|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming";		
+s_a[3] ="Autres";		
+
+
+
+function populateStates(countryElementId, stateElementId) {
+
+    var selectedCountryIndex = document.getElementById(countryElementId).selectedIndex;
+
+    var stateElement = document.getElementById(stateElementId);
+
+    stateElement.length = 0; // Fixed by Julian Woods
+    stateElement.options[0] = new Option('---', '');
+	stateElement.selectedIndex = 0;
+		
+    var state_arr = s_a[selectedCountryIndex].split("|");
+	
+    for (var i = 0; i < state_arr.length; i++) {
+        stateElement.options[stateElement.length] = new Option(state_arr[i], state_arr[i]);
+    }
+}
+
+function populateCountries(countryElementId, stateElementId) {
+    // given the id of the <select> tag as function argument, it inserts <option> tags
+    var countryElement = document.getElementById(countryElementId);
+    countryElement.length = 0;
+    countryElement.options[0] = new Option('---', '');
+	countryElement.selectedIndex = 0;
+    for (var i = 0; i < country_arr.length; i++) {
+        countryElement.options[countryElement.length] = new Option(country_arr[i], country_arr[i]);
+    }
+	
+	
+    // Assigned all countries. Now assign event listener for the states.
+
+    if (stateElementId) {
+        countryElement.onchange = function () {
+            populateStates(countryElementId, stateElementId);
+        };
+    }
+}
